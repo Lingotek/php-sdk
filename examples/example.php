6bc634350900e0324bfb143ea0b4b9598be28c1c
Flug <flugv1@gmail.com>
2017-04-21 15:14:53 +0200

    reformat project for psr-4 and fix autoloading

--- examples/example.php
+++ examples/example.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once '../src/Lingotek.php';
+require_once __DIR__. "/../vendor/autoload.php";
 
 use Lingotek\Dev;
 
@@ -8,9 +8,9 @@ $access_token = 'b068b8d9-c35b-3139-9fe2-e22ee7998d9f'; // sandbox token
 $community_id = 'f49c4fca-ff93-4f01-a03e-aa36ddb1f2b8'; // specify the "Sandbox" community
 
 $client = new \Lingotek\RestClient(array(
-  'access_token' => $access_token,
-  'base_url' => \Lingotek\RestClient::URL_SANDBOX
-    ));
+   'access_token' => $access_token,
+   'base_url' => \Lingotek\RestClient::URL_SANDBOX
+));
 
 // Get Community
 $result = $client->get('community/' . $community_id);
@@ -24,37 +24,37 @@ echo "[" . $result->info->http_code . "] community_id: $community_id (\"$communi
 $project_title = "Sample Project";
 $project_id = NULL;
 $params = array(
-  'community_id' => $community_id,
-  'limit' => 5000
+   'community_id' => $community_id,
+   'limit' => 5000
 );
 $result = $client->get('project', $params);
 $projects = $result->decoded_response->entities;
 foreach ($projects as $project) {
-  if ($project->properties->title == $project_title) { //if project name is $project_title then use the id
-    $project_id = $project->properties->id;
-    echo "[" . $result->info->http_code . "] project_id: $project_id (existing: \"" . $project->properties->title . "\")\n";
-  }
+   if ($project->properties->title == $project_title) { //if project name is $project_title then use the id
+      $project_id = $project->properties->id;
+      echo "[" . $result->info->http_code . "] project_id: $project_id (existing: \"" . $project->properties->title . "\")\n";
+   }
 }
 
 if (is_null($project_id)) {
-// Create Project (when project with $project_title was not found)
-  $params = array(
-    'title' => $project_title,
-    'community_id' => $community_id,
-    'workflow_id' => 'c675bd20-0688-11e2-892e-0800200c9a66' // machine translation workflow
-  );
-  $result = $client->post('project', $params);
-  $project_id = substr($result->headers->content_location, strrpos($result->headers->content_location, '/') + 1); // temp until API 5 makes newly created id is available in JSON body
-  echo "[" . $result->info->http_code . "] project_id: $project_id (created: \"$project_title\")\n";
+   // Create Project (when project with $project_title was not found)
+   $params = array(
+      'title' => $project_title,
+      'community_id' => $community_id,
+      'workflow_id' => 'c675bd20-0688-11e2-892e-0800200c9a66' // machine translation workflow
+   );
+   $result = $client->post('project', $params);
+   $project_id = substr($result->headers->content_location, strrpos($result->headers->content_location, '/') + 1); // temp until API 5 makes newly created id is available in JSON body
+   echo "[" . $result->info->http_code . "] project_id: $project_id (created: \"$project_title\")\n";
 }
 
 // Create Document
 $doc_params = array(
-  'title' => 'Sample Document ' . time(), // the title of the document as named on TMS
-  'content' => (object) array('title' => 'Sample Document', 'body' => 'The quick brown fox jumped over the lazy dog.'), // content for translation
-  'format' => 'JSON',
-  'locale_code' => 'en_US',
-  'project_id' => $project_id
+   'title' => 'Sample Document ' . time(), // the title of the document as named on TMS
+   'content' => (object) array('title' => 'Sample Document', 'body' => 'The quick brown fox jumped over the lazy dog.'), // content for translation
+   'format' => 'JSON',
+   'locale_code' => 'en_US',
+   'project_id' => $project_id
 );
 $result = $client->post('document', $doc_params);
 echo "\theaders->content_location: " . $result->headers->content_location . "\n";
@@ -64,90 +64,90 @@ echo "[" . $result->info->http_code . "] document_id: $document_id (created: \""
 
 // Check Import Progress
 $params = array(
-  'document_id' => $document_id
+   'document_id' => $document_id
 );
 $i = 0;
 $done_processing = FALSE;
 while (!$done_processing) {
-  $i++;
-  echo "\t#$i | check status: ";
-  try {
-    $result = $client->get('document/{document_id}/status', $params);
-  } catch (Exception $e) {
-    echo " [" . $e->getCode() . "] "; //"(" . $e->getMessage() . ")\n";
-    echo "importing\n";
-    sleep(3);
-    continue;
-  }
-  echo " [" . $result->info->http_code . "] ";
-  if (substr($result->info->http_code, 0, 1) == "2") {
-    $done_processing = TRUE;
-    echo "imported (done!)";
-  }
-  echo "\n";
-  if ($i >= 30) {
-    break;
-  }
+   $i++;
+   echo "\t#$i | check status: ";
+   try {
+      $result = $client->get('document/{document_id}/status', $params);
+   } catch (Exception $e) {
+      echo " [" . $e->getCode() . "] "; //"(" . $e->getMessage() . ")\n";
+      echo "importing\n";
+      sleep(3);
+      continue;
+   }
+   echo " [" . $result->info->http_code . "] ";
+   if (substr($result->info->http_code, 0, 1) == "2") {
+      $done_processing = TRUE;
+      echo "imported (done!)";
+   }
+   echo "\n";
+   if ($i >= 30) {
+      break;
+   }
 }
 
 // Add Translation Targets
 $locale_codes = array('zh_CN','es_MX');
 $params = array(
-  'document_id' => $document_id,
-  'workflow_id' => 'c675bd20-0688-11e2-892e-0800200c9a66' // machine translation workflow
+   'document_id' => $document_id,
+   'workflow_id' => 'c675bd20-0688-11e2-892e-0800200c9a66' // machine translation workflow
 );
 foreach ($locale_codes as $locale_code) {
-  $params['locale_code'] = $locale_code;
-  $result = $client->post('document/{document_id}/translation', $params);
-  echo "[" . $result->info->http_code . "] translation requested: $document_id => $locale_code \n";
+   $params['locale_code'] = $locale_code;
+   $result = $client->post('document/{document_id}/translation', $params);
+   echo "[" . $result->info->http_code . "] translation requested: $document_id => $locale_code \n";
 }
 
 // Check Overall Translation Progress
 $params = array(
-  'document_id' => $document_id,
+   'document_id' => $document_id,
 );
 $done = FALSE;
 $i = 0;
 while (!$done) {
-  $i++;
-  echo "\t#$i | translation progress: [" . $result->info->http_code . "] ";
-  $result = $client->get('document/{document_id}/status', $params);
-  $progress = isset($result->decoded_response->properties->progress) ? $result->decoded_response->properties->progress : "x";
-  echo $progress . "%";
-  if ($progress == 100) {
-    echo " (done!)";
-    $done = TRUE;
-  }
-  else {
-    sleep(3);
-  }
-  echo "\n";
-  if ($i >= 50) {
-    break;
-  }
+   $i++;
+   echo "\t#$i | translation progress: [" . $result->info->http_code . "] ";
+   $result = $client->get('document/{document_id}/status', $params);
+   $progress = isset($result->decoded_response->properties->progress) ? $result->decoded_response->properties->progress : "x";
+   echo $progress . "%";
+   if ($progress == 100) {
+      echo " (done!)";
+      $done = TRUE;
+   }
+   else {
+      sleep(3);
+   }
+   echo "\n";
+   if ($i >= 50) {
+      break;
+   }
 }
 
 echo "download translations: " . implode(", ", $locale_codes) . "\n";
 
 // Download Translation
 $params = array(
-  'document_id' => $document_id,
-  'locale_code' => $locale_code,
+   'document_id' => $document_id,
+   'locale_code' => $locale_code,
 );
 foreach ($locale_codes as $locale_code) {
-  $params['locale_code'] = $locale_code;
-  $result = $client->get('document/{document_id}/content', $params);
-  $translation = $result->decoded_response;
-  $translations[$locale_code] = $translation;
+   $params['locale_code'] = $locale_code;
+   $result = $client->get('document/{document_id}/content', $params);
+   $translation = $result->decoded_response;
+   $translations[$locale_code] = $translation;
 }
 
 // Resources
 $results = array(
-  'community_id' => $community_id,
-  'project_id' => $project_id,
-  'document_id' => $document_id,
-  'source' => array($doc_params['locale_code'] => $doc_params['content']),
-  'target' => $translations
+   'community_id' => $community_id,
+   'project_id' => $project_id,
+   'document_id' => $document_id,
+   'source' => array($doc_params['locale_code'] => $doc_params['content']),
+   'target' => $translations
 );
 
 echo "\n--- results ---\n";
--- src/InvalidUrlPatternException.php
+++ src/InvalidUrlPatternException.php
@@ -1,19 +1,17 @@
 <?php
 
-namespace Lingotek;
-
-require('RestClient.php');
-require('Exceptions.php');
-require('Dev.php');
-
 /**
- * Lingotek SDK
+ * Lingotek API Client
  *
- * Copyright (c) 2014 Lingotek, Inc.
+ * Copyright (c) 2014 Lingotek
  *
  * For the full copyright and license information, please view
  * the file license.md that was distributed with this source code.
  *
- *
  * Usage: readme.md
  */
+
+namespace Lingotek;
+
+class InvalidUrlPatternException extends RestClientException {
+}
